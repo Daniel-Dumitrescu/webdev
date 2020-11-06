747d71c63e788d91c0b1f8c3d129ccff79254272
Daniel Dumitrescu <ddumi062@uottawa.ca>
2020-11-05 20:53:18 -0500

    HTML, CSS, JS first commit

---
+++ Accounting_Finance_Club_Website/HTML/Scripts/leftPanel.js
@@ -0,0 +1,84 @@
+function openLessons() {
+	window.open(
+	"lessons.html",
+	"_blank"
+	)	
+}
+
+function openExercises() {
+	window.open(
+	"exercises.html",
+	"_blank"
+	)	
+}
+
+function openVideos() {
+	window.open(
+	"tutorials.html",
+	"_blank"
+	)	
+}
+
+function openAccCycle() {
+	window.open(
+	"accountingCycle.html",
+	"_blank"
+	)	
+}
+
+
+function openDescription() {
+	window.open(
+	"description.html",
+	"_blank"
+	)	
+}
+
+function openScores() {
+	window.open(
+	"scores.html",
+	"_blank"
+	)	
+}
+
+function openPlay() {
+	window.open(
+	"play.html",
+	"_blank"
+	)	
+}
+
+function openAboutClub() {
+	window.open(
+	"aboutClub.html",
+	"_blank"
+	)	
+}
+
+function openContact() {
+	window.open(
+	"contact.html",
+	"_blank"
+	)	
+}
+
+function openJoin() {
+	window.open(
+	"join.html",
+	"_blank"
+	)	
+}
+
+function openAboutCPA() {
+	window.open(
+	"aboutCPA.html",
+	"_blank"
+	)	
+}
+
+function openEvents() {
+	window.open(
+	"events.html",
+	"_blank"
+	)	
+}
